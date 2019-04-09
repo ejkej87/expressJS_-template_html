12cf8d5545c4052f4c05d85491d9ef467df6472f
ArkadiuszGarzel <ejkej87@gmail.com>
2019-04-09 18:33:11 +0200

    add sing up form

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+
+app.get('/auth/google', function(req, res) {
+   res.render('auth', {
+      user: 
+      {login: req.query.login, password: req.password}
+   });
+});
+
+
+app.get('/', function (req, res) {
+   res.render('index');
+});
+
+app.listen(3000);
+app.use(function (req, res, next) {
+   res.status(404).send("Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!")
+});
